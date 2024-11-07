309afcb98c16182554be28f6ec19f97ae741c9e7
krillkovalev <kovalyov201524@gmail.com>
2024-11-07 21:51:27 +0300

    закончил day02

--- APG1_Golang_Boot_camp/Go_Day02/src/ex03/myRotate.go
+++ APG1_Golang_Boot_camp/Go_Day02/src/ex03/myRotate.go
@@ -3,48 +3,74 @@ package main
 import (
 	"archive/tar"
 	"compress/gzip"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
+	"sync"
 )
 
 func main() {
-	files := os.Args[1:]
-	for _, file := range files {
-		info, err := os.Stat(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-		out, err := os.Create(file + fmt.Sprintf("_%s", strconv.FormatInt(info.ModTime().Unix(), 10)))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer out.Close()
 
-		err = createArchive(file+fmt.Sprintf("_%s", strconv.FormatInt(info.ModTime().Unix(), 10)), out)
-		if err != nil {
-			log.Fatal(err)
-		}
+	a := flag.Bool("a", false, "Multiple files for archiving")
+	flag.Parse()
+
+	var wg sync.WaitGroup
+	var files []string
+	if *a {
+		files = os.Args[3:]
+	} else {
+		files = append(files, os.Args[1])
+	}
+
+	for _, file := range files {
+		wg.Add(1)
+		go func(f string) {
+			defer wg.Done()
+			if *a {
+				createArchive(f, os.Args[2], true)
+			} else {
+				createArchive(f, filepath.Dir(os.Args[1]), false)
+			}
+
+		}(file)
 
 	}
+	wg.Wait()
 
 }
 
-func createArchive(file string, buf io.Writer) error {
-
-	gw := gzip.NewWriter(buf)
+func createArchive(file string, path string, flag bool) error {
+	info, err := os.Stat(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	timestamp := strconv.FormatInt(info.ModTime().Unix(), 10)
+	base_filename := filepath.Base(file)
+	out, err := os.Create(getOutputPath(path, base_filename, timestamp, flag))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	gw := gzip.NewWriter(out)
 	defer gw.Close()
 	tw := tar.NewWriter(gw)
 	defer tw.Close()
 
-	err := addToArchive(tw, file)
-	if err != nil {
-		return err
-	}
-
+	addToArchive(tw, file)
 	return nil
+}
+
+func getOutputPath(path string, base_filename string, timestamp string, flag bool) string {
+	if flag {
+		return path + "/" + strings.Replace(base_filename, ".", "_", -1) + fmt.Sprintf("_%s", timestamp) + ".tar.gz"
+
+	}
+	return path + "/" + strings.Replace(base_filename, ".", "_", -1) + fmt.Sprintf("_%s", timestamp) + ".tar.gz"
 }
 
 func addToArchive(tw *tar.Writer, filename string) error {
